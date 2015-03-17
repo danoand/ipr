849b2351754de3ca8649e4690392ed01e0f613a5
Dan Anderson <danandersonmobile@yahoo.com>
2015-03-17 10:16:11 -0500

    add logic to manipulate tech skill and skill type elements

--- static/scripts/controllers/ctrlSkillsetModalCtrl.js
+++ static/scripts/controllers/ctrlSkillsetModalCtrl.js
@@ -2,40 +2,31 @@
 appControllers.controller("SkillsetModalCtrl", ["$scope", "$rootScope", "$location", "$modalInstance",
   "svcDataPopulation", "svcTeamProgressBar", "svcSkillSelections",
   function($scope, $rootScope, $location, $modalInstance, svcDataPopulation, svcTeamProgressBar, svcSkillSelections) {
-    var techSkillSet1 = 'General Electrical';
-    var techSkillSet2 = 'Connected Home';
-    var techSkillSet3 = 'Media & Gaming Systems';
-    var techSkillSet4 = 'Security Systems';
 
-    var techSkillType1 = 'Delivery';
-    var techSkillType2 = 'Install';
-    var techSkillType3 = 'Repair';
-    var techSkillType4 = 'Training';
-
-    var initBtnStyle          = 'btn btn-block btn-default';
-    var selectedBtnStyleSkill = 'btn btn-block btn-primary';
-    var selectedBtnStyleType  = 'btn btn-block btn-warning';
+    var intBtnStyle      = 'btn btn-block btn-default';
+    var selBtnStyleSkill = 'btn btn-block btn-primary';
+    var selBtnStyleType  = 'btn btn-block btn-warning';
 
     $scope.techServiceCategory = 'Home Electronics';
-    $scope.techSkillSet1 = techSkillSet1;
-    $scope.techSkillSet2 = techSkillSet2;
-    $scope.techSkillSet3 = techSkillSet3;
-    $scope.techSkillSet4 = techSkillSet4;
+    $scope.techSkillText1 = svcSkillSelections.getBtnSkillText1();
+    $scope.techSkillText2 = svcSkillSelections.getBtnSkillText2();
+    $scope.techSkillText3 = svcSkillSelections.getBtnSkillText3();
+    $scope.techSkillText4 = svcSkillSelections.getBtnSkillText4();
 
-    $scope.techSkillType1 = techSkillType1;
-    $scope.techSkillType2 = techSkillType2;
-    $scope.techSkillType3 = techSkillType3;
-    $scope.techSkillType4 = techSkillType4;
+    $scope.techTypeText1 = svcSkillSelections.getBtnTypeText1();
+    $scope.techTypeText2 = svcSkillSelections.getBtnTypeText2();
+    $scope.techTypeText3 = svcSkillSelections.getBtnTypeText3();
+    $scope.techTypeText4 = svcSkillSelections.getBtnTypeText4();
 
-    $scope.btnTechSkillSet1 = initBtnStyle;
-    $scope.btnTechSkillSet2 = initBtnStyle;
-    $scope.btnTechSkillSet3 = initBtnStyle;
-    $scope.btnTechSkillSet4 = initBtnStyle;
+    $scope.btnSkillStyle1 = svcSkillSelections.getBtnSkillStyle(1);
+    $scope.btnSkillStyle2 = svcSkillSelections.getBtnSkillStyle(2);
+    $scope.btnSkillStyle3 = svcSkillSelections.getBtnSkillStyle(3);
+    $scope.btnSkillStyle4 = svcSkillSelections.getBtnSkillStyle(4);
 
-    $scope.btnTechSkillType1 = initBtnStyle;
-    $scope.btnTechSkillType2 = initBtnStyle;
-    $scope.btnTechSkillType3 = initBtnStyle;
-    $scope.btnTechSkillType4 = initBtnStyle;
+    $scope.btnTypeStyle1 = svcSkillSelections.getBtnTypeStyle(1);
+    $scope.btnTypeStyle2 = svcSkillSelections.getBtnTypeStyle(2);
+    $scope.btnTypeStyle3 = svcSkillSelections.getBtnTypeStyle(3);
+    $scope.btnTypeStyle4 = svcSkillSelections.getBtnTypeStyle(4);
 
     $scope.btnSkillToggle1 = svcSkillSelections.getBtnSkillToggle1();
     $scope.btnSkillToggle2 = svcSkillSelections.getBtnSkillToggle2();
@@ -62,121 +53,129 @@ appControllers.controller("SkillsetModalCtrl", ["$scope", "$rootScope", "$locati
     $scope.clickSkillBtn1 = function() {
       if ($scope.btnSkillToggle1) {
         // Skill has been de-selected, convert to default
-        $scope.btnSkillToggle1  = false;
-        $scope.btnTechSkillSet1 = initBtnStyle;
-        $scope.techSkillSet1    = techSkillSet1;
+        $scope.btnSkillToggle1 = false;
+        $scope.btnSkillStyle1  = intBtnStyle;
+        $scope.techSkillText1  = svcSkillSelections.intBtnSkillText1();
       } else {
         $scope.btnSkillToggle1  = true;
-        $scope.btnTechSkillSet1 = selectedBtnStyleSkill;
-        $scope.techSkillSet1    = techSkillSet1.toUpperCase();
+        $scope.btnSkillStyle1   = selBtnStyleSkill;
+        $scope.techSkillText1   = svcSkillSelections.intBtnSkillText1().toUpperCase();
       }
 
       svcSkillSelections.setBtnSkillToggle1($scope.btnSkillToggle1);
+      svcSkillSelections.setBtnSkillText1($scope.techSkillText1);
     };
 
     $scope.clickSkillBtn2 = function() {
       if ($scope.btnSkillToggle2) {
         // Skill has been de-selected, convert to default
         $scope.btnSkillToggle2  = false;
-        $scope.btnTechSkillSet2 = initBtnStyle;
-        $scope.techSkillSet2    = techSkillSet2;
+        $scope.btnSkillStyle2   = intBtnStyle;
+        $scope.techSkillText2   = svcSkillSelections.intBtnSkillText2();
       } else {
         $scope.btnSkillToggle2  = true;
-        $scope.btnTechSkillSet2 = selectedBtnStyleSkill;
-        $scope.techSkillSet2    = techSkillSet2.toUpperCase();
+        $scope.btnSkillStyle2   = selBtnStyleSkill;
+        $scope.techSkillText2   = svcSkillSelections.intBtnSkillText2().toUpperCase();
       }
 
       svcSkillSelections.setBtnSkillToggle2($scope.btnSkillToggle2);
+      svcSkillSelections.setBtnSkillText2($scope.techSkillText2);
     };
 
     $scope.clickSkillBtn3 = function() {
       if ($scope.btnSkillToggle3) {
         // Skill has been de-selected, convert to default
         $scope.btnSkillToggle3  = false;
-        $scope.btnTechSkillSet3 = initBtnStyle;
-        $scope.techSkillSet3    = techSkillSet3;
+        $scope.btnSkillStyle3   = intBtnStyle;
+        $scope.techSkillText3   = svcSkillSelections.intBtnSkillText3();
       } else {
         $scope.btnSkillToggle3  = true;
-        $scope.btnTechSkillSet3 = selectedBtnStyleSkill;
-        $scope.techSkillSet3    = techSkillSet3.toUpperCase();
+        $scope.btnSkillStyle3   = selBtnStyleSkill;
+        $scope.techSkillText3   = svcSkillSelections.intBtnSkillText3().toUpperCase();
       }
 
       svcSkillSelections.setBtnSkillToggle3($scope.btnSkillToggle3);
+      svcSkillSelections.setBtnSkillText3($scope.techSkillText3);
     };
 
     $scope.clickSkillBtn4 = function() {
       if ($scope.btnSkillToggle4) {
         // Skill has been de-selected, convert to default
         $scope.btnSkillToggle4  = false;
-        $scope.btnTechSkillSet4 = initBtnStyle;
-        $scope.techSkillSet4    = techSkillSet4;
+        $scope.btnSkillStyle4   = intBtnStyle;
+        $scope.techSkillText4   = svcSkillSelections.intBtnSkillText4();
       } else {
         $scope.btnSkillToggle4  = true;
-        $scope.btnTechSkillSet4 = selectedBtnStyleSkill;
-        $scope.techSkillSet4    = techSkillSet4.toUpperCase();
+        $scope.btnSkillStyle4   = selBtnStyleSkill;
+        $scope.techSkillText4   = svcSkillSelections.intBtnSkillText4().toUpperCase();
       }
 
       svcSkillSelections.setBtnSkillToggle4($scope.btnSkillToggle4);
+      svcSkillSelections.setBtnSkillText4($scope.techSkillText4);
     };
 
     $scope.clickTypeBtn1 = function() {
       if ($scope.btnTypeToggle1) {
         // Type has been de-selected, convert to default
         $scope.btnTypeToggle1    = false;
-        $scope.btnTechSkillType1 = initBtnStyle;
-        $scope.techSkillType1    = techSkillType1;
+        $scope.btnTypeStyle1     = intBtnStyle;
+        $scope.techTypeText1     = svcSkillSelections.intBtnTypeText1();
       } else {
         $scope.btnTypeToggle1    = true;
-        $scope.btnTechSkillType1 = selectedBtnStyleType;
-        $scope.techSkillType1    = techSkillType1.toUpperCase();
+        $scope.btnTypeStyle1     = selBtnStyleType;
+        $scope.techTypeText1     = svcSkillSelections.intBtnTypeText1().toUpperCase();
       }
 
       svcSkillSelections.setBtnTypeToggle1($scope.btnTypeToggle1);
+      svcSkillSelections.setBtnTypeText1($scope.techTypeText1);
     };
 
     $scope.clickTypeBtn2 = function() {
       if ($scope.btnTypeToggle2) {
         // Type has been de-selected, convert to default
-        $scope.btnTypeToggle2    = false;
-        $scope.btnTechSkillType2 = initBtnStyle;
-        $scope.techSkillType2    = techSkillType2;
+        $scope.btnTypeToggle2 = false;
+        $scope.btnTypeStyle2  = intBtnStyle;
+        $scope.techTypeText2  = svcSkillSelections.intBtnTypeText2();
       } else {
-        $scope.btnTypeToggle2    = true;
-        $scope.btnTechSkillType2 = selectedBtnStyleType;
-        $scope.techSkillType2    = techSkillType2.toUpperCase();
-
-        svcSkillSelections.setBtnTypeToggle2($scope.btnTypeToggle2);
+        $scope.btnTypeToggle2 = true;
+        $scope.btnTypeStyle2  = selBtnStyleType;
+        $scope.techTypeText2  = svcSkillSelections.intBtnTypeText2().toUpperCase();
       }
+
+      svcSkillSelections.setBtnTypeToggle2($scope.btnTypeToggle2);
+      svcSkillSelections.setBtnTypeText2($scope.techTypeText2);
     };
 
     $scope.clickTypeBtn3 = function() {
       if ($scope.btnTypeToggle3) {
         // Type has been de-selected, convert to default
-        $scope.btnTypeToggle3    = false;
-        $scope.btnTechSkillType3 = initBtnStyle;
-        $scope.techSkillType3    = techSkillType3;
+        $scope.btnTypeToggle3 = false;
+        $scope.btnTypeStyle3  = intBtnStyle;
+        $scope.techTypeText3  = svcSkillSelections.intBtnTypeText3();
       } else {
-        $scope.btnTypeToggle3    = true;
-        $scope.btnTechSkillType3 = selectedBtnStyleType;
-        $scope.techSkillType3    = techSkillType3.toUpperCase();
-
-        svcSkillSelections.setBtnTypeToggle3($scope.btnTypeToggle3);
+        $scope.btnTypeToggle3 = true;
+        $scope.btnTypeStyle3  = selBtnStyleType;
+        $scope.techTypeText3  = svcSkillSelections.intBtnTypeText3().toUpperCase();
       }
+
+      svcSkillSelections.setBtnTypeToggle3($scope.btnTypeToggle3);
+      svcSkillSelections.setBtnTypeText3($scope.techTypeText3);
     };
 
     $scope.clickTypeBtn4 = function() {
       if ($scope.btnTypeToggle4) {
         // Type has been de-selected, convert to default
-        $scope.btnTypeToggle4    = false;
-        $scope.btnTechSkillType4 = initBtnStyle;
-        $scope.techSkillType4    = techSkillType4;
+        $scope.btnTypeToggle4 = false;
+        $scope.btnTypeStyle4  = intBtnStyle;
+        $scope.techTypeText4  = svcSkillSelections.intBtnTypeText4();
       } else {
-        $scope.btnTypeToggle4    = true;
-        $scope.btnTechSkillType4 = selectedBtnStyleType;
-        $scope.techSkillType4    = techSkillType4.toUpperCase();
-
-        svcSkillSelections.setBtnTypeToggle4($scope.btnTypeToggle4);
+        $scope.btnTypeToggle4 = true;
+        $scope.btnTypeStyle4  = selBtnStyleType;
+        $scope.techTypeText4  = svcSkillSelections.intBtnTypeText4().toUpperCase();
       }
+
+      svcSkillSelections.setBtnTypeToggle4($scope.btnTypeToggle4);
+      svcSkillSelections.setBtnTypeText4($scope.techTypeText4);
     };
 
   }
--- static/scripts/services/svcSkillSelections.js
+++ static/scripts/services/svcSkillSelections.js
@@ -2,70 +2,216 @@
 //   selections made by te user
 appServices.factory("svcSkillSelections", [
 
-    function() {
-      var btnSkillToggle1 = false;
-      var btnSkillToggle2 = false;
-      var btnSkillToggle3 = false;
-      var btnSkillToggle4 = false;
+  function() {
+    var initSkillText1 = 'General Electrical';
+    var initSkillText2 = 'Connected Home';
+    var initSkillText3 = 'Media & Gaming Systems';
+    var initSkillText4 = 'Security Systems';
 
-      var btnTypeToggle1 = false;
-      var btnTypeToggle2 = false;
-      var btnTypeToggle3 = false;
-      var btnTypeToggle4 = false;
+    var initTypeText1 = 'Delivery';
+    var initTypeText2 = 'Install';
+    var initTypeText3 = 'Repair';
+    var initTypeText4 = 'Training';
 
-      return {
-        getBtnSkillToggle1: function() {
-          console.log('Getting btnSkillToggle1 which is:' + btnSkillToggle1);
-          return btnSkillToggle1;
-        },
-        getBtnSkillToggle2: function() {
-          return btnSkillToggle2;
-        },
-        getBtnSkillToggle3: function() {
-          return btnSkillToggle3;
-        },
-        getBtnSkillToggle4: function() {
-          return btnSkillToggle4;
-        },
-        setBtnSkillToggle1: function(inVal) {
-          btnSkillToggle1 = inVal;
-          console.log('Setting btnSkillToggle1 which is now:' + btnSkillToggle1);
-        },
-        setBtnSkillToggle2: function(inVal) {
-          btnSkillToggle2 = inVal;
-        },
-        setBtnSkillToggle3: function(inVal) {
-          btnSkillToggle3 = inVal;
-        },
-        setBtnSkillToggle4: function(inVal) {
-          btnSkillToggle4 = inVal;
-        },
-        getBtnTypeToggle1: function() {
-          console.log('Getting btnTypeToggle1 which is:' + btnTypeToggle1);
-          return btnTypeToggle1;
-        },
-        getBtnTypeToggle2: function() {
-          return btnTypeToggle2;
-        },
-        getBtnTypeToggle3: function() {
-          return btnTypeToggle3;
-        },
-        getBtnTypeToggle4: function() {
-          return btnTypeToggle4;
-        },
-        setBtnTypeToggle1: function(inVal) {
-          btnTypeToggle1 = inVal;
-          console.log('Setting btnTypeToggle1 which is now:' + btnTypeToggle1);
-        },
-        setBtnTypeToggle2: function(inVal) {
-          btnTypeToggle2 = inVal;
-        },
-        setBtnTypeToggle3: function(inVal) {
-          btnTypeToggle3 = inVal;
-        },
-        setBtnTypeToggle4: function(inVal) {
-          btnTypeToggle3 = inVal;
+    var techSkillText1 = initSkillText1;
+    var techSkillText2 = initSkillText2;
+    var techSkillText3 = initSkillText3;
+    var techSkillText4 = initSkillText4;
+
+    var techTypeText1 = initTypeText1;
+    var techTypeText2 = initTypeText2;
+    var techTypeText3 = initTypeText3;
+    var techTypeText4 = initTypeText4;
+
+    var btnSkillToggle1 = false;
+    var btnSkillToggle2 = false;
+    var btnSkillToggle3 = false;
+    var btnSkillToggle4 = false;
+
+    var btnTypeToggle1 = false;
+    var btnTypeToggle2 = false;
+    var btnTypeToggle3 = false;
+    var btnTypeToggle4 = false;
+
+    var initBtnStyle          = 'btn btn-block btn-default';
+    var selectedBtnStyleSkill = 'btn btn-block btn-primary';
+    var selectedBtnStyleType  = 'btn btn-block btn-warning';
+
+    return {
+      intBtnSkillText1: function() {
+        return initSkillText1;
+      },
+      intBtnSkillText2: function() {
+        return initSkillText2;
+      },
+      intBtnSkillText3: function() {
+        return initSkillText3;
+      },
+      intBtnSkillText4: function() {
+        return initTypeText4;
+      },
+      intBtnTypeText1: function() {
+        return initTypeText1;
+      },
+      intBtnTypeText2: function() {
+        return initTypeText2;
+      },
+      intBtnTypeText3: function() {
+        return initTypeText3;
+      },
+      intBtnTypeText4: function() {
+        return initTypeText4;
+      },
+      getBtnSkillText1: function() {
+        return techSkillText1;
+      },
+      getBtnSkillText2: function() {
+        return techSkillText2;
+      },
+      getBtnSkillText3: function() {
+        return techSkillText3;
+      },
+      getBtnSkillText4: function() {
+        return techSkillText4;
+      },
+      getBtnTypeText1: function() {
+        return techTypeText1;
+      },
+      getBtnTypeText2: function() {
+        return techTypeText2;
+      },
+      getBtnTypeText3: function() {
+        return techTypeText3;
+      },
+      getBtnTypeText4: function() {
+        return techTypeText4;
+      },
+      setBtnSkillText1: function(inVal) {
+        techSkillText1 = inVal;
+      },
+      setBtnSkillText2: function(inVal) {
+        techSkillText2 = inVal;
+      },
+      setBtnSkillText3: function(inVal) {
+        techSkillText3 = inVal;
+      },
+      setBtnSkillText4: function(inVal) {
+        techSkillText4 = inVal;
+      },
+      setBtnTypeText1: function(inVal) {
+        techTypeText1 = inVal;
+      },
+      setBtnTypeText2: function(inVal) {
+        techTypeText2 = inVal;
+      },
+      setBtnTypeText3: function(inVal) {
+        techTypeText3 = inVal;
+      },
+      setBtnTypeText4: function(inVal) {
+        techTypeText4 = inVal;
+      },
+      getBtnSkillStyle: function(inVal) {
+        retStyle = initBtnStyle;
+        switch (inVal) {
+          case 1:
+            if (btnSkillToggle1) {
+              retStyle = selectedBtnStyleSkill;
+            }
+            break;
+          case 2:
+            if (btnSkillToggle2) {
+              retStyle = selectedBtnStyleSkill;
+            }
+            break;
+          case 3:
+            if (btnSkillToggle3) {
+              retStyle = selectedBtnStyleSkill;
+            }
+            break;
+          case 4:
+            if (btnSkillToggle4) {
+              retStyle = selectedBtnStyleSkill;
+            }
+            break;
         }
-      };
-    }
+
+        return retStyle;
+      },
+      getBtnTypeStyle: function(inVal) {
+        retStyle = initBtnStyle;
+        switch (inVal) {
+          case 1:
+            if (btnTypeToggle1) {
+              retStyle = selectedBtnStyleType;
+            }
+            break;
+          case 2:
+            if (btnTypeToggle2) {
+              retStyle = selectedBtnStyleType;
+            }
+            break;
+          case 3:
+            if (btnTypeToggle3) {
+              retStyle = selectedBtnStyleType;
+            }
+            break;
+          case 4:
+            if (btnTypeToggle4) {
+              retStyle = selectedBtnStyleType;
+            }
+            break;
+        }
+
+        return retStyle;
+      },
+      getBtnSkillToggle1: function() {
+        return btnSkillToggle1;
+      },
+      getBtnSkillToggle2: function() {
+        return btnSkillToggle2;
+      },
+      getBtnSkillToggle3: function() {
+        return btnSkillToggle3;
+      },
+      getBtnSkillToggle4: function() {
+        return btnSkillToggle4;
+      },
+      setBtnSkillToggle1: function(inVal) {
+        btnSkillToggle1 = inVal;
+      },
+      setBtnSkillToggle2: function(inVal) {
+        btnSkillToggle2 = inVal;
+      },
+      setBtnSkillToggle3: function(inVal) {
+        btnSkillToggle3 = inVal;
+      },
+      setBtnSkillToggle4: function(inVal) {
+        btnSkillToggle4 = inVal;
+      },
+      getBtnTypeToggle1: function() {
+        return btnTypeToggle1;
+      },
+      getBtnTypeToggle2: function() {
+        return btnTypeToggle2;
+      },
+      getBtnTypeToggle3: function() {
+        return btnTypeToggle3;
+      },
+      getBtnTypeToggle4: function() {
+        return btnTypeToggle4;
+      },
+      setBtnTypeToggle1: function(inVal) {
+        btnTypeToggle1 = inVal;
+      },
+      setBtnTypeToggle2: function(inVal) {
+        btnTypeToggle2 = inVal;
+      },
+      setBtnTypeToggle3: function(inVal) {
+        btnTypeToggle3 = inVal;
+      },
+      setBtnTypeToggle4: function(inVal) {
+        btnTypeToggle4 = inVal;
+      }
+    };
+  }
 ]);

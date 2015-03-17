e152873e42c19f317fa4563f095590c9d4738985
Dan Anderson <danandersonmobile@yahoo.com>
2015-03-17 11:17:19 -0500

    add logic to admin page
    displaying elements based on whether the Admin is performing service or
    not

--- static/scripts/controllers/ctrlAdministratorCtrl.js
+++ static/scripts/controllers/ctrlAdministratorCtrl.js
@@ -1,6 +1,6 @@
 // AdministratorCtrl is a controller for the administrator.html view
-appControllers.controller("AdministratorCtrl", ["$scope", "$rootScope", "svcDataPopulation", "svcDataHTML", "svcAdminPart",
-  function($scope, $rootScope, svcDataPopulation, svcDataHTML, svcAdminPart) {
+appControllers.controller("AdministratorCtrl", ["$scope", "$rootScope", "$modal", "svcDataPopulation", "svcDataHTML", "svcAdminPart",
+  function($scope, $rootScope, $modal, svcDataPopulation, svcDataHTML, svcAdminPart) {
     svcDataHTML.setCompanyName(svcDataHTML.htmlCompanyName);
     svcDataHTML.setCompanyIcon(svcDataHTML.htmlCompanyIcon);
     $rootScope.$emit('evCompanyData');
@@ -11,10 +11,13 @@ appControllers.controller("AdministratorCtrl", ["$scope", "$rootScope", "svcData
 
     $rootScope.$emit('evStatusData');
 
-    $scope.companyUsername = svcDataPopulation.companyUsername();
-    $scope.companyEmail = svcDataPopulation.companyEmail();
+    $scope.companyUsername      = svcDataPopulation.companyUsername();
+    $scope.companyEmail         = svcDataPopulation.companyEmail();
     $scope.companyFullNameFirst = svcDataPopulation.companyFullNameFirst();
-    $scope.companyFullNameLast = svcDataPopulation.companyFullNameLast();
+    $scope.companyFullNameLast  = svcDataPopulation.companyFullNameLast();
+
+    $scope.adminPart         = { yes: false, no: false };
+    $scope.techSkillCategory = '-- Select Skill Category --';
 
     $scope.virtualType = function() {
       $scope.companyMiddleName = svcDataPopulation.companyMiddleName();
@@ -26,6 +29,28 @@ appControllers.controller("AdministratorCtrl", ["$scope", "$rootScope", "svcData
       svcAdminPart.setAdminPartFlag($scope.adminPartVal);
 
       $rootScope.$emit('evAdminIsParticipating');
+
+      if ($scope.adminPartVal == 'yes') {
+        $scope.adminPart.yes = true;
+      } else {
+        $scope.adminPart.yes = false;
+      }
+
+      if ($scope.adminPartVal == 'no') {
+        $scope.adminPart.no = true;
+      } else {
+        $scope.adminPart.no = false;
+      }
+    };
+
+    $scope.techSkillCategory = '-- Select Skill Category --';
+
+    $scope.openSkillsetModal = function() {
+      var modalInstanceInsurance = $modal.open({
+        size: 'lg',
+        templateUrl: "myAdminModalSkillset.html",
+        controller: 'SkillsetModalCtrl'
+      });
     };
 
   }

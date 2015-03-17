7839789168ff0ca81f2064bda2b439f4166d2ff6
Dan Anderson <danandersonmobile@yahoo.com>
2015-03-17 17:53:21 -0500

    interim checkin

---
+++ static/scripts/controllers/ctrlWrapUpCtrl.js
@@ -0,0 +1,27 @@
+// BusinessProfileCtrl is a controller for the 06_businessprofile.html view
+appControllers.controller("AdminBackgroundCtrl", ["$scope", "$rootScope", "$location", "svcDataPopulation", "svcDataHTML", "svcDataGaugeChart", "svcAdminPart",
+  function($scope, $rootScope, $location, svcDataPopulation, svcDataHTML, svcDataGaugeChart, svcAdminPart) {
+    svcDataHTML.setCompanyName(svcDataHTML.htmlCompanyName);
+    svcDataHTML.setCompanyIcon(svcDataHTML.htmlCompanyIcon);
+    $rootScope.$emit('evCompanyData');
+
+    svcDataHTML.setAdminName(svcDataHTML.htmlAdminName);
+    svcDataHTML.setAdminIcon(svcDataHTML.htmlAdminIcon);
+    $rootScope.$emit('evAdminData');
+
+    $scope.wrapupGaugeProfileData = svcDataGaugeChart.wrapupGaugeProfileData();
+    $scope.wrapupGaugeProfileOpts = svcDataGaugeChart.wrapupGaugeProfileOpts();
+    $scope.wrapupGaugeTechData    = svcDataGaugeChart.wrapupGaugeTechData();
+    $scope.wrapupGaugeTechOpts    = svcDataGaugeChart.wrapupGaugeTechOpts();
+
+    $scope.adminPart = svcAdminPart.getAdminPartFlag();
+    if ($scope.adminPart == 'no') {
+      $scope.optAdminPartYes = false;
+      $scope.optAdminPartNo  = true;
+    } else {
+      $scope.optAdminPartYes = true;
+      $scope.optAdminPartNo  = false;
+    }
+
+  }
+]);
--- static/scripts/services/svcDataGaugeChart.js
+++ static/scripts/services/svcDataGaugeChart.js
@@ -128,6 +128,62 @@ appServices.factory("svcDataGaugeChart", [
               [1, "#A3C86D"]
             ]
           };
+        },
+        wrapupGaugeProfileData: function() {
+          return {
+            maxValue: 800,
+            animationSpeed: 20,
+            val: 700
+          };
+        },
+        wrapupGaugeProfileOpts: function() {
+          return {
+            lines: 12,
+            angle: 0,
+            lineWidth: .47,
+            pointer: {
+              length: .6,
+              strokeWidth: .03,
+              color: "#000000"
+            },
+            limitMax: "true",
+            colorStart: "#7ACBEE",
+            colorStop: "#7ACBEE",
+            strokeColor: "#F1F1F1",
+            generateGradient: !0,
+            percentColors: [
+              [0, "#7ACBEE"],
+              [1, "#7ACBEE"]
+            ]
+          };
+        },
+        wrapupGaugeTechData: function() {
+          return {
+            maxValue: 100,
+            animationSpeed: 20,
+            val: 75
+          };
+        },
+        wrapupGaugeTechOpts: function() {
+          return {
+            lines: 12,
+            angle: 0,
+            lineWidth: .47,
+            pointer: {
+              length: .6,
+              strokeWidth: .03,
+              color: "#000000"
+            },
+            limitMax: "true",
+            colorStart: "#A3C86D",
+            colorStop: "#A3C86D",
+            strokeColor: "#E0E0E0",
+            generateGradient: !0,
+            percentColors: [
+              [0, "#A3C86D"],
+              [1, "#A3C86D"]
+            ]
+          };
         }
       };
     }
